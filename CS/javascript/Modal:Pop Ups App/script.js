155da8785e044daabfe819e6015899d68103bbda
RODNEY MARSHALL JR <rodneymarshallhgs.org@TheHiddProject3-540.lan>
2024-10-12 12:42:14 -0700

    saturday push2

---
+++ CS/javascript/Modal:Pop Ups App/script.js
@@ -0,0 +1,28 @@
+//Get modal element
+const modal = document.getElementById("myModal");
+//Get buttons
+const openModaBtn = ducumnet.getElementById("openModaBtn");
+const closeModaBtn = ducumnet.getElementById("openModaBtn");
+const alertBtn = ducumnet.getElementById("alertBtn");
+
+
+//Open modal on button click
+openModalBtn.onclick = funtion() {
+    modal.style.display = "block";
+}
+
+//Open modal on button click
+openModalBtn.onclick = funtion() {
+    modal.style.display = "none";
+}
+
+//Open modal on button click
+openModalBtn.onclick = funtion() {
+    if (event.target ==modal) {
+    modal.style.display = "none";
+}
+
+//Open modal on button click
+openModalBtn.onclick = funtion() {
+    alert("This is a simple pop-ip alert! 2k is a way better game now. GO 2k!")
+}
