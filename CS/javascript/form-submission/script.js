b0c955e0357e48a4016493b0850363c3d7e0aee2
RodIsTall <rodney.marshall@hgs.hiddengeniusproject.org>
2024-11-25 18:01:59 -0800

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,24 @@
-function formSubmission() {
-    // Your code here.
-}
+document.addEventListener("DOMContentLoaded" ,  () => {
+    const form = document.getElementById('myForm');
+
+    form.addEventListener('sumbit', function(event) {
+        event.preventDefault();
+        //Add Variables from index.html (hint; Use document.get function)
+
+        // Get the values from the input fields
+        const name = document.getElementById ('name').value;
+        const email = document.getElementById('email').value; 
+        const message = document.getElementById('message').value;
+
+
+
+
+        if (name && email && message) {
+            alert(`From submitted successfully!\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
+        } else {  
+            alert('Please fill all the fields');
+        }    
+    });
+});
+//Add additional fields
+
